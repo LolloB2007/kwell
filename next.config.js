0dd2d4106c17d39dce84f3395fe0084d3ed0e459
Lorenzo <lollo.barbagelata@gmail.com>
2026-05-30 14:36:44 +0200

    Deploy Next.js site to GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+
+  output: "export",
+
+  images: {
+
+    unoptimized: true,
+
+  },
+
+  basePath: "/kwell_website",
+
+  assetPrefix: "/kwell_website/",
+
+};
+
+module.exports = nextConfig;
+
